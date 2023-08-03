640070bfe5ef0e84394191e12dfa00b8c1004673
Andrei Cocan <123215462+AMCocan@users.noreply.github.com>
2023-08-02 23:55:52 -0400

    Copilot Code Demo

--- Script/Default.js
+++ Script/Default.js
@@ -1,2 +1,20 @@
 // Default JS for Markdown Site...
 
+// function readMarkdownFile(filename) { ... }
+function readMarkdownFile(filename) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", filename, false);
+    rawFile.onreadystatechange = function() {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                var markdown = rawFile.responseText;
+                var html = marked(markdown);
+                document.getElementById("mkdown").innerHTML = html;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+// Call the function with the file name
+readMarkdownFile("markdown.md");
